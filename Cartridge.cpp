453d05a70afee3ee435d0e42e3a685c36449aab2
Jonathan Maldonado Contreras <jonathan.mcontreras@gmail.com>
2019-05-23 22:03:57 +0100

    Use rom file name instead of a hardcoded path to load the rom

--- Cartridge.cpp
+++ Cartridge.cpp
@@ -28,16 +28,21 @@ bool Cartridge::IsLoaded() const
     return isLoaded;
 }
 
-bool Cartridge::TryLoad(const char* romFile)
+bool Cartridge::TryLoad( const char* romFile )
 {
-    std::basic_ifstream<byte> cartridgeFile("roms/donkey.nes", std::ios::binary | std::ios::in | std::ios::ate);
+    if ( romFile == nullptr )
+    {
+        return false;
+    }
+
+    std::basic_ifstream<byte> cartridgeFile( romFile, std::ios::binary | std::ios::in | std::ios::ate );
     if ( cartridgeFile.is_open() )
     {
         cartridgeSize = cartridgeFile.tellg();
-        cartridgeFile.seekg(0);
+        cartridgeFile.seekg( 0 );
 
-        rom = new byte[cartridgeSize];
-        cartridgeFile.read(rom, cartridgeSize);
+        rom = new byte[ cartridgeSize ];
+        cartridgeFile.read( rom, cartridgeSize );
     }
     cartridgeFile.close();
 
@@ -63,29 +68,29 @@ bool Cartridge::TryLoadHeader()
     }
 
     ui64 constant = 0;
-    for (byte i = 0x00; i <= 0x03; ++i)
+    for ( byte i = 0x00; i <= 0x03; ++i )
     {
         constant <<= 8;
         constant = constant | rom[i];
     }
 
-    if (constant != ROM_CONSTANT_HEADER)
+    if ( constant != ROM_CONSTANT_HEADER )
     {
         return false;
     }
 
-    header.prgRomSizeKB = static_cast<ui32>(rom[0x04]) * 16;
-    header.chrRomSizeKB = static_cast<ui32>(rom[0x05]) * 8;
+    header.prgRomSizeKB = static_cast<ui32>( rom[ 0x04 ] ) * 16;
+    header.chrRomSizeKB = static_cast<ui32>( rom[ 0x05 ] ) * 8;
 
     /* Flags 6 */
-    header.mirroringType = MirroringType(rom[0x06] & 0b0000'0001);
-    header.hasPRGRam = rom[0x06] & 0b0000'0010;
-    header.has512BTrainer = rom[0x06] & 0b0000'0100;
-    header.ignoreMirroring = rom[0x06] & 0b0000'1000;
-    const byte lowerNibbleMapper = (rom[0x06] & 0b1111'0000) >> 4;
+    header.mirroringType = MirroringType( rom[ 0x06 ] & 0b0000'0001 );
+    header.hasPRGRam = rom[ 0x06 ] & 0b0000'0010;
+    header.has512BTrainer = rom[ 0x06 ] & 0b0000'0100;
+    header.ignoreMirroring = rom[ 0x06 ] & 0b0000'1000;
+    const byte lowerNibbleMapper = ( rom[ 0x06 ] & 0b1111'0000 ) >> 4;
     
     /* Flags 7 */
-    const byte upperNibbleMapper = rom[0x07] & 0b1111'0000;
+    const byte upperNibbleMapper = rom[ 0x07 ] & 0b1111'0000;
     header.mapper = upperNibbleMapper | lowerNibbleMapper;
 
     // TODO (Jonathan): Implement support for iNES 2.0
@@ -97,12 +102,12 @@ void Cartridge::PrintDetails() const
         << "Cartridge file Size (KB) " << cartridgeSize << "\n" 
         << "PRG ROM Size (KB): " << header.prgRomSizeKB << "\n"
         << "CHR ROM Size (KB): " << header.chrRomSizeKB<< "\n"
-        << "Mirroring: " << MirroringTypeString[static_cast<byte>(header.mirroringType)] << "\n"
+        << "Mirroring: " << MirroringTypeString[ static_cast< byte >( header.mirroringType ) ] << "\n"
         << "Has persistent memory: " << header.hasPersistentMemory << "\n"
         << "Contains PRG RAM: " << header.hasPRGRam << "\n"
         << "Contains 512B trainer: " << header.has512BTrainer << "\n"
         << "Ignores Mirroring: " << header.ignoreMirroring << "\n"
-        << "Mapper: " << static_cast<ui32>(header.mapper);
+        << "Mapper: " << static_cast< ui32 >( header.mapper );
 
     std::cout << std::endl;
 }
